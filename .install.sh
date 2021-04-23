394c4559a663b06b91d7ef2789f00a6100e307c6
james-t-larson <timothy_james@tuta.io>
2021-04-23 21:31:19 +0200

    many many things changed

--- .install.sh
+++ .install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-pacman -Syu python anki alacritty tmux vifm code nautilus firefox discord steam ttf-font-awesome unzip feh
+pacman -Syu python anki alacritty tmux vifm code nautilus firefox rofi ttf-font-awesome unzip feh python-pip bitwarden
+	# discord
+	# steam
 	# flatpaks?
 	# teams?
 	# spotify?
@@ -9,46 +11,72 @@ pamac remove nitrogen
 ###############
 # SET GTK THEME
 ###############
-
+ 
 # create themes dir
-mkdir ~/.themes
+mkdir /home/james/.themes
 # downloading gtk dracula theme
-curl https://codeload.github.com/dracula/gtk/zip/master --output ~/.themes/Dracula.zip
+curl https://codeload.github.com/dracula/gtk/zip/master --output /home/james/.themes/Dracula.zip
 
 # unzip to correct file
-unzip ~/.themes/Dracula.zip -d ~/.themes/Dracula
+unzip /home/james/.themes/Dracula.zip -d /home/james/.themes/Dracula
 
 # remove zip
-rm ~/.themes/Dracula.zip
+rm /home/james/.themes/Dracula.zip
 
 #rename theme to dracula
-mv ~/.themes/gtk-master ~/.themes/Dracula
+mv /home/james/.themes/gtk-master ~/.themes/Dracula
+
 
 ###############
 # SET ICON PACK
 ###############
 
 # downloading dracula icon pack
-curl "https://github-repository-files.githubusercontent.com/118868345/5214870?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20210422%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20210422T114040Z&amp;X-Amz-Expires=300&amp;X-Amz-Signature=32534b3a45181f04e846951d14e3a99617881add97d49e2436bad5450068d4cb&amp;X-Amz-SignedHeaders=host&amp;actor_id=0&amp;key_id=0&amp;repo_id=118868345&amp;response-content-disposition=attachment%3Bfilename%3DDracula.zip&amp;response-content-type=application%2Fzip" --output ~/.icons/Dracula.zip
+wget -O /home/james/.icons/Dracula.zip https://github.com/dracula/gtk/files/5214870/Dracula.zip
 
-unzip ~/.icons/Dracula.zip -d ~/.icons/Dracula
+unzip /home/james/.icons/Dracula -d /home/james/.icons/Dracula
 
 # remove zip file
-rm ~/.icons/Dracula.zip
+rm /home/james/.icons/Dracula.zip
 
 # remane default config files 
-mv ~/.bashrc ~/.bashrc.default
-mv ~/.config/dunst/dunstrc ~/.config/dunst/dunstrc.default
-mv ~/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini.default
-mv ~/.config/vifm/vifmrc ~/.config/vifm/vifmrc.default
-mv ~/.i3/config ~/.i3/config.default
+mv /home/james/.bashrc /home/james/.bashrc.default
+mv /home/james/.config/dunst/dunstrc /home/james/.config/dunst/dunstrc.default
+mv /home/james/.config/gtk-3.0/settings.ini /home/james/.config/gtk-3.0/settings.ini.default
+mv /home/james/.config/vifm/vifmrc /home/james/.config/vifm/vifmrc.default
+mv /home/james/.i3/config /home/james/.i3/config.default
+
+############
+# SETUP TMUX
+############
 
 # clone tmux plugin package manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm /home/james/.tmux/plugins/tpm
+
+# install plugins for tmux
+/home/james/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+##########
+# SETUP i3 
+##########
+
+# installing icons daemon for i3
+pip3 install i3-workspace-names-daemon
+
+# install better tiling defaults
+pip install autotiling
+
+##############
+# GET DOTFILES
+##############
 
 pamac build yadm
 
-# clone repo
+yadm init
 
-# run script for tmux plugin install
+# pull dotfiles down
+yadm pull https://github.com/james-t-larson/dotfiles
+
+# restart i3 to make changes
+i3-msg restart
 
